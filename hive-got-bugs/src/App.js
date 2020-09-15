46bf43eb37765ce7a808253b468cc178ba22812f
David John <davidspc@MacBook-Air.local>
2020-09-15 15:15:57 +0100

    navbar buttons linked to app router

--- hive-got-bugs/src/App.js
+++ hive-got-bugs/src/App.js
@@ -3,17 +3,16 @@ import "./App.css";
 import { Router } from "@reach/router";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
-import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <NavBar />
+
       <Router>
-        <Home path="/homepage" />
-        <Dashboard path="/dashboard" />
+        {/* <Home path="/homepage" />
+        <Dashboard path="/dashboard"/> */}
       </Router>
     </div>
   );
--- hive-got-bugs/src/components/Header.jsx
+++ hive-got-bugs/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Title from "./Title";
 import Icon from "./Icon";
+import NavBar from "./NavBar";
 
 const Header = () => {
   return (
     <div className="homeContainer">
       <Icon />
+      <NavBar />
       <div className="header">
         <Title />
       </div>
