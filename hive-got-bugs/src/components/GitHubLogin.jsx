a4c09273d31809ec0cee7b5fb78e2329c7574d6b
David John <davidspc@MacBook-Air.local>
2020-09-15 11:10:11 +0100

    toggle viewer completed and github login complete

---
+++ hive-got-bugs/src/components/GitHubLogin.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC2veJu3sp0kAhZtjCB05PY9_ceQI1ROe0",
+  authDomain: "hive-got-bugs.firebaseapp.com",
+});
+
+class GitHubLogin extends Component {
+  state = {
+    isSignedIn: false,
+  };
+
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ isSignedIn: !!user });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed in</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default GitHubLogin;
