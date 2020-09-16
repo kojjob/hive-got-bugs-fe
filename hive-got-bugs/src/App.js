4aedcb743235aada110406a87356ec02219a5d0b
David John <davidspc@MacBook-Air.local>
2020-09-16 10:59:22 +0100

    bug chart component made

--- hive-got-bugs/src/App.js
+++ hive-got-bugs/src/App.js
@@ -4,13 +4,14 @@ import { Router } from "@reach/router";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
 import Header from "./components/Header";
-import GitHubLogin from "./components/GitHubLogin";
+import BugChart from "./components/BugChart";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <GitHubLogin />
+
+      <BugChart />
       <Router>
         {/* <Home path="/homepage" />
         <Dashboard path="/dashboard"/> */}
--- hive-got-bugs/src/components/BugChart.jsx
+++ hive-got-bugs/src/components/BugChart.jsx
@@ -1,1 +1,52 @@
-//function here
+import React, { Component } from "react";
+import { Bar } from "react-chartjs-2";
+
+class BugChart extends Component {
+  state = {
+    isLoading: true,
+    bugPointsOverMonth: 0,
+  };
+
+  componentDidMount() {
+    //apiFunc(()=> {})
+    this.setState({ isLoading: false });
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    const data = {
+      labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+      datasets: [
+        {
+          label: "Bug points every 30 days",
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+          ],
+          borderColor: "rgba(255,99,132,1)",
+          borderWidth: 1,
+          hoverBackgroundColor: "rgba(255,100,64,0.4)",
+          hoverBorderColor: "rgba(255,99,132,1)",
+          data: [13, 17, 25, 33],
+        },
+      ],
+    };
+    if (isLoading) return <p>Bug chart is loading...</p>;
+    return (
+      <div>
+        <Bar
+          data={data}
+          width={100}
+          height={300}
+          options={{
+            maintainAspectRatio: false,
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default BugChart;
