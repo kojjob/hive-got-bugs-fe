02327b65bd6e4e66b0a36d2b12a6a3770cba33f0
David John <davidspc@MacBook-Air.local>
2020-09-16 15:19:34 +0100

    styled button and Link  component added

--- hive-got-bugs/src/App.js
+++ hive-got-bugs/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import { Router } from "@reach/router";
 import Header from "./components/Header";
-import Dashboard from "./components/Dashboard";
+import { StyledHexButton } from "./styled/lib";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Dashboard user="Jane Blogs" />
 
+      <StyledHexButton />
       <Router></Router>
     </div>
   );
--- hive-got-bugs/src/components/NavBar.jsx
+++ hive-got-bugs/src/components/NavBar.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import ToggleViewer from "./ToggleViewer";
-import { Link } from "@reach/router";
+import { StyledLink } from "../styled/lib";
 import Search from "./Search";
 
 function NavBar() {
   return (
     <div className="navbar">
       <ToggleViewer label={"Menu"}>
-        <Link to="/homepage">
-          <button>Home</button>
-        </Link>
-        <Link to="/dashboard">
-          <button>Dashboard</button>
-        </Link>
-        <Link to="/become-a-mentor">
-          <button>Become a mentor</button>
-        </Link>
-        <Link to="/mentors">
-          <button>Mentors</button>
-        </Link>
+        <StyledLink as="button" to="/homepage">
+          Home
+        </StyledLink>
+        <StyledLink as="button" to="/dashboard">
+          Dashboard
+        </StyledLink>
+        <StyledLink as="button" to="/become-a-mentor">
+          Become a mentor
+        </StyledLink>
+        <StyledLink as="button" to="/mentors">
+          Mentors
+        </StyledLink>
       </ToggleViewer>
       <ToggleViewer label="Search" default={false}>
         <Search />
