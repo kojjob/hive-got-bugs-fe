0f0247cea27bdecc04a1506e9236314a5a4f3147
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-09-17 14:44:33 +0100

    Temporarily disable search component

--- hive-got-bugs/src/components/Search.jsx
+++ hive-got-bugs/src/components/Search.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { MDBCol } from "mdbreact";
+// import React from "react";
+// import { MDBCol } from "mdbreact";
 
-const Search = () => {
-  return (
-    <div>
-      <form>
-        <MDBCol md="6">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-          />
-        </MDBCol>
-      </form>
-    </div>
-  );
-};
+// const Search = () => {
+//   return (
+//     <div>
+//       <form>
+//         <MDBCol md="6">
+//           <input
+//             className="form-control"
+//             type="text"
+//             placeholder="Search"
+//             aria-label="Search"
+//           />
+//         </MDBCol>
+//       </form>
+//     </div>
+//   );
+// };
 
-export default Search;
+// export default Search;
